719750ac4152c56f60116de32e9f7fab24440320
october-rain <572626298@qq.com>
2022-06-14 19:31:30 +0800

    feat: finish serve.ts

--- mini-serve/serve.ts
+++ mini-serve/serve.ts
@@ -3,10 +3,7 @@ import path from "node:path";
 import serveHandler from "./handler";
 import arg from "arg";
 import { isNumeric } from "./utils";
-import Config from "./interface";
 
-const warning = (message: string) => `WARNING: ${message}`;
-const info = (message: string) => `INFO: ${message}`;
 const error = (message: string) => `ERROR: ${message}`;
 const getHelp = () => `
     mini-serve - 这是一个简单的静态资源服务器
@@ -14,7 +11,35 @@ const getHelp = () => `
     xxx 省略10000字
 `;
 
-function startEndpoint(port: number, config: Config, args: any) {};
+function startEndpoint(port: number, entry: string) {
+  const server = http.createServer(
+    (req: IncomingMessage, res: ServerResponse) => {
+      console.log("start 执行");
+      serveHandler();
+    }
+  );
+
+  server.on('error', err => {
+    // 表示端口号已被占用
+    if ((err as any).code === 'EADDRINUSE') {
+      setTimeout(() => {
+        server.close();
+        startEndpoint(port + 1, entry)
+      }, 0)
+			return
+		}
+
+		process.exit(1);
+  })
+
+  server.listen(port, () => {
+    console.log(`
+    Server is listening on port ${port}
+    Open http://localhost:${port}
+`);
+  });
+
+}
 
 // 解析命令行参数 -p 的值的含义
 const parseEndpoint = (endpoint: string) => {
@@ -22,10 +47,12 @@ const parseEndpoint = (endpoint: string) => {
     return Number(endpoint);
   }
 
+  // todo 其他情况
   // ...
 };
 
-const loadConfig = async function (cwd: string, entry: string, args: any) {};
+// todo 加载配置
+// const loadConfig = async function (cwd: string, entry: string, args: any) {};
 
 (async () => {
   let args = null;
@@ -43,10 +70,18 @@ const loadConfig = async function (cwd: string, entry: string, args: any) {};
     process.exit(1);
   }
 
-  const cwd = process.cwd();
-  const entry = args["--entry"] ? path.resolve(args["--entry"]) : cwd;
+  if (args["--help"]) {
+    console.log(getHelp());
+    return;
+  }
 
-  const config = await loadConfig(cwd, entry, args);
+  // todo
+  // 其他参数
 
-  // startEndpoint(args['--port'] ?? 3000, args['--entry'] ?? 'index.html')
+  // todo 加载配置
+  // const cwd = process.cwd();
+  // const entry = args["--entry"] ? path.resolve(args["--entry"]) : cwd;
+  // const config = await loadConfig(cwd, entry, args);
+
+  startEndpoint(args['--port'] ?? 3000, args['--entry'] ?? 'index.html')
 })();
